fa8c55305e209e0088065e643632ddfdf8909a15
Tharindu Wijerathne <tharindusachinthaka4@gmail.com>
2024-03-28 17:10:07 +0530

    Initial commit

---
+++ DLL Tutorial/DLL Tutorial/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using TestDLL;
+
+namespace TutorialDLL
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number : ");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter second number : ");
+            int y = int.Parse(Console.ReadLine());
+
+            int resultDevision = MathSevice.Devide(x, y);
+            Console.WriteLine("Result is "+resultDevision);
+
+            Console.Read();
+        }
+    }
+}
---
+++ DLL Tutorial/TestDLL/MathSevice.cs
@@ -0,0 +1,48 @@
+﻿namespace TestDLL
+{
+    public static class MathService
+    {
+        public static int Divide(int x, int y)
+        {
+            if (y == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return x / y;
+        }
+
+        public static int Multiply(params int[] numbers)
+        {
+            if (numbers.Length == 0)
+            {
+                throw new ArgumentException("At least one number must be provided.");
+            }
+
+            int result = 1;
+            foreach (int num in numbers)
+            {
+                result *= num;
+            }
+            return result;
+        }
+
+        public static int Add(params int[] numbers)
+        {
+            int sum = 0;
+            foreach (int num in numbers)
+            {
+                sum += num;
+            }
+            return sum;
+        }
+
+        public static int Subtract(int minuend, params int[] subtrahends)
+        {
+            foreach (int num in subtrahends)
+            {
+                minuend -= num;
+            }
+            return minuend;
+        }
+    }
+}
