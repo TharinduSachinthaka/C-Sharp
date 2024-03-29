59983ed9d6729295e67d5fe114f21ad3b9ec4111
Tharindu Wijerathne <tharindusachinthaka4@gmail.com>
2024-03-29 17:03:43 +0530

    March 29

---
+++ Calculator/DecryptClassLibrary/LicenseDecryptor.cs
@@ -0,0 +1,36 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace DecryptClassLibrary
+{
+    public class LicenseDecryptor
+    {
+        // Decrypt function using AES-256-GCM algorithm
+        public static string Decrypt(string encryptedData, string key)
+        {
+            byte[] encryptedBytes = Convert.FromBase64String(encryptedData);
+
+            // Extract IV and tag from the encrypted data
+            byte[] iv = encryptedBytes[..12]; // IV length is 12 bytes
+            byte[] tag = encryptedBytes[12..28]; // GCM tag length is 16 bytes
+            byte[] cipherText = encryptedBytes[28..]; // Rest is cipher text
+
+            // Convert key to bytes
+            byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+
+            // Create AesGcm instance
+            using AesGcm aesGcm = new AesGcm(keyBytes);
+
+            // Create plaintext buffer
+            byte[] decryptedBytes = new byte[cipherText.Length];
+
+            // Decrypt the data
+            aesGcm.Decrypt(iv, cipherText, tag, decryptedBytes);
+
+            // Convert decrypted bytes to string
+            string decryptedData = Encoding.UTF8.GetString(decryptedBytes);
+
+            return decryptedData;
+        }
+    }
+}
---
+++ SideBar Application/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace SideBar_Application
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(32, 27, 204);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(894, 66);
+            panel1.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.BackColor = Color.FromArgb(51, 46, 209);
+            flowLayoutPanel1.Dock = DockStyle.Left;
+            flowLayoutPanel1.Location = new Point(0, 66);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(200, 472);
+            flowLayoutPanel1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(894, 538);
+            Controls.Add(flowLayoutPanel1);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            Name = "Form1";
+            Text = "Side Bar";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private FlowLayoutPanel flowLayoutPanel1;
+    }
+}
